adce0ce5731699ac1d208df8eb18096564dc4efb
Santiago Font <santifonlop@hotmail.com>
2024-08-18 14:36:40 -0300

    more pages

--- app/trabajos/page.jsx
+++ app/trabajos/page.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import SideBar from '../Components/sidebar/page'
-
+import style from './trabajos.module.css'
 export default function Trabajos() {
   return (
-    <div>
+    <div class={style.trabajos_wrap}>
         <SideBar />
+        <div className={style.trabajos}>
+          <div className={style.trabajos_header}>
+            <div className="trabajos_btton">Agregar trabajo</div>
+            <div className="trabajos_btton">Buscar</div>
+            {/* <div className="trabajos_btton">Agregar trabajo</div>
+            <div className="trabajos_btton">Agregar trabajo</div> */}
+          </div>
+        </div>
     </div>
   )
 }
